e53970122e26e619afdb0757ea44c47799d01373
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-10-21 20:05:16 +0300

    added update, delete and get to-do-items methods in repository

--- src/main/java/org/fasttrackit/todolist/persistance/ToDoItemRepository.java
+++ src/main/java/org/fasttrackit/todolist/persistance/ToDoItemRepository.java
@@ -1,12 +1,12 @@
 package org.fasttrackit.todolist.persistance;
 
+import org.fasttrackit.todolist.domain.ToDoItem;
 import org.fasttrackit.todolist.transfer.CreateToDoItemRequest;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ToDoItemRepository {
 
@@ -22,5 +22,53 @@ public class ToDoItemRepository {
 
             preparedStatement.executeUpdate();
         }
+    }
+
+    public void updateToDoItem(long id, boolean done) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "UPDATE to_do_item SET done=? WHERE id=?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setBoolean(1, done);
+            preparedStatement.setLong(2, id);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public void deleteToDoItem(long id) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "DELETE FROM to_do_item WHERE id=?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public List<ToDoItem> getToDoItems() throws SQLException, IOException, ClassNotFoundException {
+        String sql = "SELECT id, description, deadline, done FROM to_do_item";
+
+        List<ToDoItem> toDoItems = new ArrayList<>();
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
+            while (resultSet.next()) {
+                ToDoItem toDoItem = new ToDoItem();
+
+                toDoItem.setId(resultSet.getLong("id"));
+                toDoItem.setDescription(resultSet.getString("description"));
+                toDoItem.setDeadline(resultSet.getDate("deadline").toLocalDate());
+                toDoItem.setDone(resultSet.getBoolean("done"));
+
+                toDoItems.add(toDoItem);
+            }
+
+        }
+
+        return toDoItems;
     }
 }
