0ea9faa09940c6ae73ea16d1d9d6dec3317bec5d
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-10-28 21:14:46 +0200

    refactored object mapper configuration

--- src/main/java/org/fasttrackit/todolist/web/ToDoItemServlet.java
+++ src/main/java/org/fasttrackit/todolist/web/ToDoItemServlet.java
@@ -1,7 +1,6 @@
 package org.fasttrackit.todolist.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.fasttrackit.todolist.config.ObjectMapperConfiguration;
 import org.fasttrackit.todolist.domain.ToDoItem;
 import org.fasttrackit.todolist.service.ToDoItemService;
 import org.fasttrackit.todolist.transfer.CreateToDoItemRequest;
@@ -25,11 +24,9 @@ public class ToDoItemServlet extends HttpServlet {
     // endpoint
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
         CreateToDoItemRequest request =
-                objectMapper.readValue(req.getReader(), CreateToDoItemRequest.class);
+                ObjectMapperConfiguration.getObjectMapper()
+                        .readValue(req.getReader(), CreateToDoItemRequest.class);
 
         try {
             toDoItemService.createToDoItem(request);
@@ -54,11 +51,9 @@ public class ToDoItemServlet extends HttpServlet {
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getParameter("id");
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
         UpdateToDoItemRequest request =
-                objectMapper.readValue(req.getReader(), UpdateToDoItemRequest.class);
+                ObjectMapperConfiguration.getObjectMapper()
+                        .readValue(req.getReader(), UpdateToDoItemRequest.class);
 
         try {
             toDoItemService.updateToDoItem(Long.parseLong(id), request);
@@ -72,10 +67,8 @@ public class ToDoItemServlet extends HttpServlet {
         try {
             List<ToDoItem> toDoItems = toDoItemService.getToDoItems();
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.registerModule(new JavaTimeModule());
-
-            String response = objectMapper.writeValueAsString(toDoItems);
+            String response = ObjectMapperConfiguration.getObjectMapper()
+                    .writeValueAsString(toDoItems);
 
             resp.getWriter().print(response);
         } catch (SQLException | ClassNotFoundException e) {
